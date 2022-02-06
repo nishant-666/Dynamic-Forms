8ed88bc0cc73db79aec1fc07227493141afefcbe
Nishant kumar <nishants440@gmail.com>
2022-02-06 14:40:37 +0530

    Dynamic Form Fields

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [formFields, setFormFields] = useState([
+    { name: '', age: '' },
+  ])
+
+  const handleFormChange = (event, index) => {
+    let data = [...formFields];
+    data[index][event.target.name] = event.target.value;
+    setFormFields(data);
+  }
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(formFields)
+  }
+
+  const addFields = () => {
+    let object = {
+      name: '',
+      age: ''
+    }
+
+    setFormFields([...formFields, object])
+  }
+
+  const removeFields = (index) => {
+    let data = [...formFields];
+    data.splice(index, 1)
+    setFormFields(data)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submit}>
+        {formFields.map((form, index) => {
+          return (
+            <div key={index}>
+              <input
+                name='name'
+                placeholder='Name'
+                onChange={event => handleFormChange(event, index)}
+                value={form.name}
+              />
+              <input
+                name='age'
+                placeholder='Age'
+                onChange={event => handleFormChange(event, index)}
+                value={form.age}
+              />
+              <button onClick={() => removeFields(index)}>Remove</button>
+            </div>
+          )
+        })}
+      </form>
+      <button onClick={addFields}>Add More..</button>
+      <br />
+      <button onClick={submit}>Submit</button>
     </div>
   );
 }
